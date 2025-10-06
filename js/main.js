8c467ff7f0b121165274981321f5909bc8044cf2
Ziad.Abuolila <ziad.abuolila@gmail.com>
2025-10-06 11:12:59 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementById("heart").addEventListener("click", function() {
+    let name = document.getElementById("name");
+    name.style.opacity = "1";
+    name.style.top = "30%";
+});
