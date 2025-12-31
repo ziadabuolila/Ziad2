c140a1d5b5f82b242aae8ec74cb393e2402226d6
Ziad.Abuolila <ziad.abuolila@gmail.com>
2025-12-31 22:06:26 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-const heart = document.getElementById("heart");
-const names = document.querySelectorAll(".name");
+document.addEventListener("DOMContentLoaded", () => {
 
-heart.addEventListener("click", () => {
-    names.forEach((el, index) => {
-        setTimeout(() => {
-            el.style.opacity = "1";
-            el.style.transform = "translateY(0)";
-        }, index * 700);
+    const heart = document.getElementById("heart");
+    const names = document.querySelectorAll(".name");
+
+    heart.addEventListener("click", () => {
+        names.forEach((el, index) => {
+            setTimeout(() => {
+                el.style.opacity = "1";
+                el.style.transform = "translateY(0)";
+            }, index * 700);
+        });
     });
+
 });
